181a9fb00443b5c6f567f41d91477ab84c9fe7e0
Stephan Zlatarev <stephan.zlatarev@sap.com>
2020-10-13 22:28:17 +0300

    Progress bar with improved look and feel

--- docs/code/facility/control/ProgressBar.js
+++ docs/code/facility/control/ProgressBar.js
@@ -3,14 +3,37 @@ import { Hub } from '../../Hub.js';
 export class ProgressBar {
 
   constructor() {
-    let bar = $("<div>");
+    this.bar = $("<div>").css("width", "100%").css("height", "100%");
+    this.blocks = 20;
+
+    Hub.on("routine", "loaded", function(routine) {
+      this.blocks = routine.size();
+    }.bind(this));
 
     Hub.on("practice", "completed", function(data) {
-      $("<span>&#x25CF;</span>")
-        .css("color", data.success ? "green" : "red")
-        .appendTo(bar);
-      Hub.push("footer", "active", bar);
-    });
+      this.bar.append(block(100 / this.blocks, data.success ? "green" : "red"));
+      Hub.push("footer", "active", this.bar);
+    }.bind(this));
   }
 
+};
+
+let gradient = function(color) {
+  if (color === "green") {
+    return "linear-gradient(to bottom, #A3EFA3 35%, #60C04F)";
+  } else if (color === "red") {
+    return "linear-gradient(to bottom, #EFA3A3 35%, #C04F4F)";
+  }
+
+  return "linear-gradient(to bottom, #A3E2EF 35%, #4F9CC0)";
+};
+
+let block = function(size, color) {
+  return $("<div>")
+    .css("display", "block").css("float", "left")
+    .css("margin-left", "1px")
+    .css("width", size + "%").css("height", "100%")
+    .css("background", gradient(color))
+    .css("box-shadow", "0 0 8px 1px white inset")
+    .css("border-radius", "0.4rem");
 };
