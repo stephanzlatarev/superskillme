d5875475f4f9f26ea147c4d0ad45a60a794d99ab
Stephan Zlatarev <stephan.zlatarev@sap.com>
2020-10-13 12:52:57 +0300

    Troubleshooting local storage on mobile

--- docs/code/facility/Refresher.js
+++ docs/code/facility/Refresher.js
@@ -4,6 +4,7 @@
 export class Refresher {
 
   constructor(htmlElement) {
+    alert("storage:" + localStorage);
     if (localStorage) {
       let element = htmlElement;
       checkVersion(element);
@@ -14,11 +15,14 @@ export class Refresher {
 }
 
 let checkVersion = function(element) {
+  alert("head /");
   $.ajax({
     type: "HEAD",
     url: "/",
     success: function(data, status, request) {
+      alert("got response:" + request);
       let lastModified = request.getResponseHeader("last-modified");
+      alert("got last modified:" + lastModified + " vs " + localStorage.appversion);
       if (lastModified === localStorage.appversion) {
         element.empty().append("Version: " + lastModified);
       } else {
